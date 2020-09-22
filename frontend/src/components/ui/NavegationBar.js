bf124ae1244cd316874b903a1c73507811d89f0b
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-09-22 17:17:57 -0300

    Update NavegationBar.js

--- frontend/src/components/ui/NavegationBar.js
+++ frontend/src/components/ui/NavegationBar.js
@@ -3,42 +3,41 @@ import { Link, NavLink } from 'react-router-dom'
 
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import Button from 'react-bootstrap/Button';
-
+import Button from 'react-bootstrap/Button'
 
 import { AddTodoModal }  from '../modal/AddTodoModal'
 
 export const NavegationBar = () => {
     return (
         <>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar collapseOnSelect  bg="dark" variant="dark">
                 
                 <Link to="/"><Navbar.Brand >TODO LIST</Navbar.Brand></Link>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-
-                    <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">SeparatedLink</NavDropdown.Item>
-                    </NavDropdown>
-                    </Nav>
-                    <Nav>
-                        <NavLink 
-                            activeClassName="active"
-                            className="nav-item nav-link" 
-                            exact
-                            to="/archived"
-                        >
-                            Archivos
+    
+                <Nav className="mr-auto">
+                    
+                    <NavLink 
+                        activeClassName="active"
+                        className="nav-item nav-link" 
+                        exact   to="/archived"
+                    >
+                        Archivos
                     </NavLink>
-                    </Nav>
-                </Navbar.Collapse>
-                <Button>Boton</Button>
+
+                    <NavLink 
+                        activeClassName="active"
+                        className="nav-item nav-link" 
+                        exact   to="/search"
+                    >
+                        Buscar
+                    </NavLink>
+                    
+                </Nav>
+
+                <Nav>
+                    <Button>Agregar Tarea</Button>
+                </Nav>
+               
             </Navbar>
             <AddTodoModal />
         </>

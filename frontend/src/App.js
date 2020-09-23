2d4c41253ac48dc820e2f1d47b9034bdafeb33e4
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-09-23 12:53:37 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,13 @@ import React from 'react'
 import { AppRouter } from './routers/AppRouter'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+
 export const App = () => {
+    
+    
+    
+    
     return (
         <div>
             <AppRouter />

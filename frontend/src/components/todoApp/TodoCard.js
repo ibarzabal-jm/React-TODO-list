f5d176453e793e81e63dc884953f7ec1d9e03cc7
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-09-23 11:19:19 -0300

    Update TodoCard.js

--- frontend/src/components/todoApp/TodoCard.js
+++ frontend/src/components/todoApp/TodoCard.js
@@ -1,23 +1,39 @@
 import React from 'react'
+import { Button, Card, Col } from 'react-bootstrap';
 
 export const TodoCard = ({ todo, index, handleDelete, handleArchive}) => {
+
+
+    const{ id, desc, category, done } = todo;
+
+
     return (
-        <li>
-            <p 
-                className={ `${ todo.done && 'complete' }`}
-                onClick={ ()=>handleArchive( todo.id ) }
-            
-            >
-                {index + 1}. { todo.desc }
-            </p>
+        <Col md={4} lg={3} xl={2} className="mb-4 text-center d-flex align-items-stretch justify-content-center">
+            <Card >
+
+                <Card.Body>
+                    <Card.Title>
+                        {category}
+                    </Card.Title>
+
+                    <Card.Text>
+                        {desc}
+                    </Card.Text>
+
+                </Card.Body>
 
 
-            <button 
-                className="btn btn-danger"
-                onClick={ ()=>handleDelete( todo.id ) }                                        
-            >
-                Borrar
-            </button>
-        </li>
+                <Card.Footer>
+
+                    {
+                        done?
+                            <Button onClick={ () => handleDelete( id ) }> Borrar Definitivamente </Button>
+                            :<Button onClick={ () => handleArchive( id ) }> Completar </Button>                            
+
+                    }
+
+                </Card.Footer>
+            </Card>
+        </Col>
     )
 }

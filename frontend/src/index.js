0f1fc950fe084747b1d5baa30cae7a1a4244b115
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-09-24 21:31:52 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import { TodoApp } from './TodoApp';
 
 
 
 
 ReactDOM.render(
-    <App />,
+    <TodoApp />,
   document.getElementById('root')
 );
 

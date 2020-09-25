edb07291cbf5f153ca5b1991606ff5f09c8392a6
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-09-24 21:32:01 -0300

    Update TodoCard.js

--- frontend/src/components/todoApp/TodoCard.js
+++ frontend/src/components/todoApp/TodoCard.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Button, Card, Col } from 'react-bootstrap';
 
+
 export const TodoCard = ({ todo, index, handleDelete, handleArchive}) => {
 
 
     const{ id, desc, category, done } = todo;
-
-
+    
+ 
     return (
         <Col className="mb-4 text-center d-flex align-items-stretch justify-content-center">
             <Card >
@@ -17,6 +18,11 @@ export const TodoCard = ({ todo, index, handleDelete, handleArchive}) => {
                     </Card.Title>
 
                     <Card.Text>
+
+                        {/* <img src={url} alt={title} /> */}
+
+
+
                         {desc}
                     </Card.Text>
 
